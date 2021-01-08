7ebf54e76a1353dff40a14f3e7e8fa3317470580
Bikash Dhakal <dhakalbikash0@gmail.com>
2021-01-08 23:09:38 +0545

    hotel router testing

--- tddTesting/hotelRouter.test.js
+++ tddTesting/hotelRouter.test.js
@@ -13,7 +13,7 @@ app.use('/hotel', auth.verifyUser, hotelRouter);
 require('./setup');
 
 let token;
-let hotelId;
+let hotelID;
 beforeAll(() => {
     return request(app).post('/users/register')
         .send({
@@ -54,8 +54,83 @@ beforeAll(() => {
             services: [],
          })
          .then((res)=> {
-             hotelId = res.body._id
+             hotelID = res.body._id
              expect(res.statusCode).toBe(201);
          })
      })
+     test('should be able to get hotel', ()=> {
+        return request (app).get('/Hotel')
+        .set('authorization', token)
+        .then((res)=> {
+            
+            expect(res.statusCode).toBe(200);
+        })
+    })
+    test('Should not be able to register hotel details', () => {
+         return request(app).post('/Hotel')
+         .set('authorization', token)
+         .send({
+            gallery: [],
+            room: [],
+            
+            hotelName: "bikash53",
+            contact: "1234567",
+            email: "dhakalbikash0@gmail.com",
+            description: "my hotel",
+            address: [],
+            services: [],
+         })
+         .then((res)=> {
+             expect(res.statusCode).toBe(401);
+         })
+     })
+     test('should be able to get donation details by id', () => {
+        return request(app).get(`/hotel/${hotelID}`)
+        .set('authorization', token)
+        .then((res)=>{
+            
+            expect(res.statusCode).toBe(200);
+        })
+    })
+
+    test('should be able to update donation details', ()=>{
+        return request(app).put(`/hotel/${hotelID}`)
+        .set('authorization', token)
+        .send({
+            gallery: [],
+            room: [],
+            
+            hotelName: "bikash5y3",
+            contact: "1234567",
+            email: "dhakalbikash0@gmail.com",
+            description: "my hotel",
+            address: [],
+            services: [],
+        })
+
+        .then((res)=>{
+            
+            expect(res.statusCode).toBe(200);
+        })
+    })
+    test('should be able to get donation details by id', () => {
+        return request(app).get(`/hotel/${hotelID}/address`)
+        .set('authorization', token)
+        .then((res)=>{
+            
+            expect(res.statusCode).toBe(200);
+        })
+    })
+    test('Should be able to register hotel details', () => {
+        return request(app).post(`/hotel/${hotelID}/address`)
+        .set('authorization', token)
+        .send({
+           street: 'ddd',
+           country: 'kk',
+           state:'ee'
+        })
+        .then((res)=> {
+            expect(res.statusCode).toBe(201);
+        })
+    })
  })
